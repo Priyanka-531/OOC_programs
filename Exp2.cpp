ca731faaac9a1498f1bb321ad8a3fef561f76fd9
Priyanka Jadhav <psjrck531@gmail.com>
2026-09-21 10:40:31 +0530

    Rename Experiment no.2 to Exp2.cpp

---
+++ Exp2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+
+class complex
+{
+ 
+public:
+int real;
+int ima;
+complex()
+{
+ real = 0;
+ ima = 0;
+}
+
+
+complex(int i, int j)
+{
+  real=i;
+  ima=j;
+}
+void display(complex num1, complex num2)
+{
+real =num1.real+num2.real;
+ima = num1.ima+num2.ima;
+cout<<"The sum is : "<<real<<"+"<<ima<<"i"<<endl;
+}
+
+};
+
+
+int main()
+{
+complex c1(5,10);
+complex c2(7,14);
+
+complex sum;
+
+
+sum.display(c1,c2);
+return 0;
+}
